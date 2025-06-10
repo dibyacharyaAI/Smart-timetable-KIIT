36a0d932d7780a28af60b0fa476e2ff7c66dbfc0
dibyacharyaAI <dibyakanta.acharya@dhammai.com>
2025-06-10 09:36:27 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 from flask import Flask, request, jsonify
 import pandas as pd
 from timetable_pipeline.formatter import (
